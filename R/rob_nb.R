bac0a2a10c6bb36614f4ee948a0768f53fae31eb
Tim Barry <tbarry2@andrew.cmu.edu>
2025-09-02 17:22:45 -0400

    add weights option

--- R/rob_nb.R
+++ R/rob_nb.R
@@ -50,21 +50,21 @@ ai.sig.tukey <- function(mui,sigma,c.tukey){
   }
 }
 
-sig.rob.tukey <- function(sigma,y,mu,c.tukey){
+sig.rob.tukey <- function(sigma,y,mu,c.tukey,weights){
   r <- (y-mu)/sqrt(varfunc(mu,sigma))
   wi <- tukeypsi(r=r,c.tukey=c.tukey)/r
-  sum(wi * psi.sig.ML(r = r, mu = mu, sigma = sigma) - ai.sig.tukey(mu, sigma, c.tukey))
+  sum(weights * wi * psi.sig.ML(r = r, mu = mu, sigma = sigma)) - sum(weights) * ai.sig.tukey(mu, sigma, c.tukey)
 }
 
 ############
 # UNIVARIATE
 ############
-fit_rob_nb_univariate <- function(y, c.tukey.beta=10, c.tukey.sigma=10, minsig=1e-3, maxsig=50, minmu=1e-10, maxmu=1e20, maxit=50, tol=1e-5, maxit.sig=30, tol.sig=1e-6, warn=FALSE){
+fit_rob_nb_univariate <- function(y, weights = rep(1, length(y)), c.tukey.beta=10, c.tukey.sigma=10, minsig=1e-3, maxsig=50, minmu=1e-10, maxmu=1e20, maxit=50, tol=1e-5, maxit.sig=30, tol.sig=1e-6, warn=FALSE){
   n <- length(y)
   #-------------------------------------------------------------------
   # MLEs of both sigma and beta
   #-------------------------------------------------------------------
-  fit <- MASS::glm.nb(formula = y ~ 1)
+  fit <- MASS::glm.nb(formula = y ~ 1, weights = weights)
   sigma <- 1/fit$theta
   mu <- exp(fit$coefficients[[1]])
   eta <- link(mu)
@@ -81,7 +81,7 @@ fit_rob_nb_univariate <- function(y, c.tukey.beta=10, c.tukey.sigma=10, minsig=1
     beta00 <- beta11
     # estimate sigma given mu
     if (update.sigma) {
-      tryit <- try(uniroot(f=sig.rob.tukey,interval=c(minsig,maxsig),tol=tol.sig,maxiter=maxit.sig,mu=mu,y=y,c.tukey=c.tukey.sigma),T)
+      tryit <- try(uniroot(f=sig.rob.tukey,interval=c(minsig,maxsig),tol=tol.sig,maxiter=maxit.sig,mu=mu,y=y,c.tukey=c.tukey.sigma,weights=weights),T)
       if (class(tryit)=='try-error') {
         if (warn){message(paste('warning: robust update of sigma failed at iteration ',it,', returning last value',sep=''))}
         update.sigma <- FALSE
@@ -97,11 +97,11 @@ fit_rob_nb_univariate <- function(y, c.tukey.beta=10, c.tukey.sigma=10, minsig=1
     it.mu <- 0
     while (max(abs(beta1-beta0)) > tol & it.mu < maxit) {
       beta0 <- beta1
-      bi <- E.tukeypsi.2(mui = mu, sigma = sigma, c.tukey = c.tukey.beta) * varfunc(mu = mu, sigma = sigma)^(-3/2) * mu^2
+      bi <- weights * E.tukeypsi.2(mui = mu, sigma = sigma, c.tukey = c.tukey.beta) * varfunc(mu = mu, sigma = sigma)^(-3/2) * mu^2
       ei <- (tukeypsi(r=(y - mu)/sqrt(varfunc(mu, sigma)), c.tukey = c.tukey.beta) - E.tukeypsi.1(mui = mu, sigma = sigma, c.tukey = c.tukey.beta))/
         E.tukeypsi.2(mui = mu, sigma = sigma, c.tukey = c.tukey.beta) * varfunc(mu, sigma) * derivlink(mu)
       zi <- eta + ei
-      eta <- beta1 <- sum(zi * bi)/(n * bi)
+      eta <- beta1 <- sum(zi * bi)/sum(bi)
       mu <- invlink(eta)
       if (mu > maxmu) mu <- maxmu
       if (mu < minmu) mu <- minmu
