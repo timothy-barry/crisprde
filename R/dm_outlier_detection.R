ef53713e29cf5ab0ff239085a2f5751fc39c702b
Tim Barry <tbarry2@andrew.cmu.edu>
2025-09-08 17:35:33 -0400

    work on plotting for dm

--- R/dm_outlier_detection.R
+++ R/dm_outlier_detection.R
@@ -10,11 +10,12 @@
 #' @export
 #'
 #' @examples
-#' umi_tab_fp <- "/Users/timbarry/research_offsite/external/crispr-quant/guideseq/count_tables/293T-SpRY-Cas9-dsODN-only-GSPneg-S81_S89_L001_count_table.rds"
+#' umi_tab_fp <- "/Users/timbarry/research_offsite/external/crispr-quant/guideseq/count_tables/293T-SpRY-Cas9-1620-GSPneg-S79_S87_L001_count_table.rds"
 #' count_df <- readRDS(umi_tab_fp)
+#' res <- find_guideseq_edit_sites_dm_sliding_window(count_df)
 find_guideseq_edit_sites_dm_sliding_window <- function(count_df, window_size = 25, multiplicity_adjustment = "BH",
-                                                       chrs_to_keep = seq(1L, 22L), plot_col = c("dodgerblue3", "firebrick")[1]) {
-  count_df <- count_df |> dplyr::filter(chr %in% chrs_to_keep)
+                                                       multiplicity_alpha = 0.1, chrs_to_keep = seq(1L, 22L), fit = NULL, plot_col = c("dodgerblue3", "firebrick")[1]) {
+  count_df <- count_df |> dplyr::filter(chr %in% chrs_to_keep) |> dplyr::ungroup()
   n_loci <- nrow(count_df)
   # obtain X matrix
   X <- sapply(X = seq(1, n_loci), FUN = function(i) {
@@ -34,11 +35,78 @@ find_guideseq_edit_sites_dm_sliding_window <- function(count_df, window_size = 2
                                                   coord <= (curr_coord + offset)) |> dplyr::pull(count) |> sum()
     c(k_minus_2, k_minus_1, k_0, k_plus_1, k_plus_2)
   }) |> t()
+
   # fit model
-  fit <- fit_dm_model(X)
-  # compute p-values
+  if (is.null(fit)) {
+    print("Fitting DM model.")
+    fit <- fit_dm_model(X)
+  }
+
+  # compute p-values and update count_df
   p_values <- compute_lrt_p_vals(X = X, pi = fit$pi, theta = fit$theta)
-  count_df$p_value <- p_values
-  out <- list(count_df = count_df, count_matrix = X, fit = fit)
+  res_df <- count_df |> dplyr::mutate(p_value = p_values)
+
+  # partition the genome into 100-bp windows; compute clustered result
+  res_df <- compute_distances_between_occupied_loci(res_df, 20)
+  clustered_res_df <- res_df |>
+    dplyr::group_by(group_id) |>
+    dplyr::summarize(p_combined = combine_p_values_simes(p_value))
+  clustered_res_df$significant_hit <- p.adjust(p = clustered_res$p_combined, method = "BH") < 0.1
+
+  # make plots
+  # 1. manhattan plot
+  manhattan_plot <- make_manhattan_plot(res_df)
+  # 2. p-value histogram
+  p_value_histogram <- make_p_value_histogram(clustered_res)
+  # 3. global scatterplots
+  global_scatterplot_linear <- make_scatterplot(count_df = count_df, x_range = NULL,
+                                                facet_on_chr = TRUE, log_trans = FALSE, col = plot_col)
+  global_scatterplot_log <- make_scatterplot(count_df = count_df, x_range = NULL,
+                                             facet_on_chr = TRUE, log_trans = TRUE, col = plot_col)
+  # 4. zoomed in plots of significant sites
+  zoomed_plots <- make_discovery_site_scatterplots_dm(res_df, clustered_res_df)
+
+  out <- list(res_df = res_df, clustered_res_df = clustered_res_df, count_matrix = X, fit = fit)
   return(out)
+}
+
+combine_p_values_simes <- function(ps, alpha = 0.1) {
+  sorted_ps <- sort(ps)
+  n <- length(sorted_ps)
+  min(n * sorted_ps/seq(1, n))
+}
+
+# clustering loci; computing distances between occupied loci
+compute_distances_between_occupied_loci <- function(count_df, thresh = 20) {
+  # 1. simple distance
+  count_df_w_dist <- count_df |>
+    dplyr::group_by(chr) |>
+    dplyr::mutate(simple_dist = c(NA, diff(coord)))
+  curr_group_id <- 0L
+  ds <- count_df_w_dist$simple_dist
+  group_id <- integer(length = nrow(count_df))
+  for (i in seq(1, nrow(count_df))) {
+    if (ds[i] > thresh || is.na(ds[i])) {
+      curr_group_id <- curr_group_id + 1
+    }
+    group_id[i] <- curr_group_id
+  }
+  count_df_w_dist <- count_df_w_dist |>
+    dplyr::ungroup() |> dplyr::mutate(group_id = group_id)
+
+  # 2. min distance
+  ds <- lapply(X = unique(count_df$chr), FUN = function(curr_chr) {
+    coords <- count_df |> dplyr::filter(chr == curr_chr) |> dplyr::pull(coord)
+    if (length(coords) == 1) {
+      1e7
+    } else {
+      dif_v <- diff(coords)
+      first_d <- dif_v[1]
+      last_d <- dif_v[length(dif_v)]
+      middle_d <- pmin(dif_v[seq(1, length(dif_v) - 1)], dif_v[seq(2, length(dif_v))])
+      c(first_d, middle_d, last_d)
+    }
+  }) |> unlist()
+  count_df_w_dist$min_dist <- ds
+  return(count_df_w_dist)
 }
--- R/plotting.R
+++ R/plotting.R
@@ -20,7 +20,8 @@ make_scatterplot <- function(count_df, x_range = NULL, facet_on_chr = FALSE, log
   }
   # custom x-range?
   if (!is.null(x_range)) {
-    suppressMessages(p <- p + ggplot2::xlim(x_range) + ggplot2::scale_x_continuous(breaks = seq(x_range[1], x_range[2], by = 1)))
+    p <- p + ggplot2::scale_x_continuous(breaks = seq(x_range[1], x_range[2], by = 1),
+                                         limits = c(x_range[1], x_range[2]))
   }
   if (!is.null(title)) {
     p <- p + ggplot2::ggtitle(title)
@@ -34,21 +35,21 @@ make_discovery_site_scatterplots <- function(count_df, result_df, plot_window_si
   result_df_sig <- result_df |> dplyr::filter(significant_hit)
   if (nrow(result_df_sig) >= 1) {
     plot_list <- lapply(seq(1, nrow(result_df_sig)), function(i) {
-      curr_lead_base <- result_df_sig$lead_base[i]
-      curr_chr <- as.character(result_df_sig$chr[i])
-      count_df_sub <- count_df |>
-        dplyr::mutate(coord = coord - curr_lead_base) |>
-        dplyr::filter(coord >= -plot_window_size/2 & coord <= plot_window_size/2 & chr == curr_chr)
-      p_log <- make_scatterplot(count_df = count_df_sub,
-                                x_range = c(-plot_window_size/2, plot_window_size/2),
-                                facet_on_chr = FALSE, log_trans = TRUE, col = col,
-                                title = paste0("Chr", curr_chr, ":", curr_lead_base))
-      p_linear <- make_scatterplot(count_df = count_df_sub,
-                                   x_range = c(-plot_window_size/2, plot_window_size/2),
-                                   facet_on_chr = FALSE, log_trans = FALSE, col = col,
-                                   title = paste0("Chr", curr_chr, ":", curr_lead_base))
-      list(p_log = p_log, p_linear = p_linear)
-    })
+    curr_lead_base <- result_df_sig$lead_base[i]
+    curr_chr <- as.character(result_df_sig$chr[i])
+    count_df_sub <- count_df |>
+      dplyr::mutate(coord = coord - curr_lead_base) |>
+      dplyr::filter(coord >= -plot_window_size/2 & coord <= plot_window_size/2 & chr == curr_chr)
+    p_log <- make_scatterplot(count_df = count_df_sub,
+                              x_range = c(-plot_window_size/2, plot_window_size/2),
+                              facet_on_chr = FALSE, log_trans = TRUE, col = col,
+                              title = paste0("Chr", curr_chr, ":", curr_lead_base))
+    p_linear <- make_scatterplot(count_df = count_df_sub,
+                                 x_range = c(-plot_window_size/2, plot_window_size/2),
+                                 facet_on_chr = FALSE, log_trans = FALSE, col = col,
+                                 title = paste0("Chr", curr_chr, ":", curr_lead_base))
+    list(p_log = p_log, p_linear = p_linear)
+  })
     lead_base_names <- paste0("Chr", result_df_sig$chr, ":", result_df_sig$lead_base)
     log_plots <- lapply(X = plot_list, FUN = function(l) l[["p_log"]]) |> setNames(lead_base_names)
     linear_plots <- lapply(X = plot_list, FUN = function(l) l[["p_linear"]]) |> setNames(lead_base_names)
@@ -59,7 +60,6 @@ make_discovery_site_scatterplots <- function(count_df, result_df, plot_window_si
 
   return(out)
 }
-
 
 make_histogram_plot <- function(y, fit, result_df, model) {
   # compute fitted density
@@ -99,5 +99,60 @@ make_histogram_plot <- function(y, fit, result_df, model) {
     ggplot2::scale_y_continuous(transform = scales::pseudo_log_trans(), breaks = c(0, 10^seq(0, 5))) +
     ggplot2::ggtitle("Log y-axis")
   out <- list(plot_untrans = p_model_untrans, plot_trans = p_model_trans)
+  return(out)
+}
+
+make_manhattan_plot <- function(res_df) {
+  p <- ggplot2::ggplot(data = res_df |>
+                         dplyr::mutate(p_value = ifelse(p_value < 1e-300, 1e-300, p_value)),
+                       mapping = ggplot2::aes(x = coord, y = p_value)) +
+    ggplot2::geom_point(cex = 0.5) +
+    ggplot2::scale_y_continuous(transform = sceptre:::revlog_trans(),
+                                breaks = 10^(-seq(0, 1000, by = 100))) +
+    ggplot2::facet_wrap("chr") + ggplot2::theme_bw() +
+    ggplot2::theme(axis.text.x = ggplot2::element_blank()) +
+    ggplot2::xlab("Coordinate") + ggplot2::ylab("P-value")
+  return(p)
+}
+
+make_p_value_histogram <- function(clustered_res) {
+  p_thresh <- if (sum(clustered_res$significant_hit) >= 1) {
+    clustered_res |> dplyr::filter(significant_hit) |> dplyr::pull(p_combined) |> max()
+  } else {
+    NULL
+  }
+  p_vals <- clustered_res$p_combined
+  p_model_untrans <- ggplot2::ggplot(data = data.frame(x = p_vals), mapping = ggplot2::aes(x = x)) +
+    ggplot2::geom_histogram(binwidth = 0.01, col = "black", fill = "black") +
+    ggplot2::theme_bw() + ggplot2::ylab("Frequency") + ggplot2::xlab("P-value") +
+    ggplot2::geom_vline(xintercept = p_thresh, col = "firebrick3")
+  return(p_model_untrans)
+}
+
+make_discovery_site_scatterplots_dm <- function(res_df, clustered_res_df, plot_window_size = 24) {
+  signif_group_ids <- clustered_res_df |> dplyr::filter(significant_hit) |> dplyr::pull(group_id)
+  # loop through significant group ids
+  plot_list <- lapply(signif_group_ids, function(curr_group_id) {
+    curr_res_sub <- res_df |> dplyr::filter(group_id == curr_group_id)
+    curr_lead_base <- curr_res_sub$coord[which.max(curr_res_sub$count)]
+    curr_chr <- curr_res_sub$chr[which.max(curr_res_sub$chr)]
+    count_df_sub <- res_df |>
+      dplyr::mutate(coord = coord - curr_lead_base) |>
+      dplyr::filter(coord >= -plot_window_size/2 & coord <= plot_window_size/2 & chr == curr_chr) |>
+      dplyr::select(chr, coord, count)
+    plot_title <- paste0("Chr", curr_chr, ":", curr_lead_base)
+    p_log <- make_scatterplot(count_df = count_df_sub,
+                              x_range = c(-plot_window_size/2, plot_window_size/2),
+                              facet_on_chr = FALSE, log_trans = TRUE, col = col,
+                              title = plot_title)
+    p_linear <- make_scatterplot(count_df = count_df_sub,
+                                 x_range = c(-plot_window_size/2, plot_window_size/2),
+                                 facet_on_chr = FALSE, log_trans = FALSE, col = col,
+                                 title = plot_title)
+    list(p_log = p_log, p_linear = p_linear)
+  })
+  log_plots <- lapply(X = plot_list, FUN = function(l) l[["p_log"]]) |> setNames(paste0("group:", signif_group_ids))
+  linear_plots <- lapply(X = plot_list, FUN = function(l) l[["p_linear"]]) |> setNames(paste0("group:", signif_group_ids))
+  out <- list(log_plots = log_plots, linear_plots = linear_plots)
   return(out)
 }
