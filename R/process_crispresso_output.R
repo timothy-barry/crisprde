cd04cc6e60727f991fafd9e0c9df0a2c1a571f24
Tim Barry <tbarry2@andrew.cmu.edu>
2026-04-02 13:35:39 -0400

    update ; use the entire allele table (i.e., not just restricted to around the cut site), but do not try to compute the position of the mutation (only the length).

--- R/process_crispresso_output.R
+++ R/process_crispresso_output.R
@@ -4,21 +4,23 @@
 #'
 #' @param allele_table_fp the file path to the allele count table
 #'
-#' @returns a data frame with columns `allele_id` (giving the aligned sequence concatenated to the reference sequence, mutation_type (insertion, deletion, or complex), and mutation length)
+#' @returns a data frame with columns `allele_id` (giving the aligned sequence concatenated to the reference sequence) `mutation_type `(insertion, deletion, or complex), `mutation_length`, `read_count`, and `mutated`. Alleles classified by CRISPResso as "unmodified" are collapsed into a single allele labeled "unmutated."
 #' @export
 #'
 #' @examples
-#' # allele_table_fp <- "/Users/timbarry/research_offsite/external/bauer-lab/rhampseq_bcl11a/hiseq1_crispresso_output/GE0423-edited/CRISPResso_on_1450_OT_0000/Alleles_frequency_table_around_sgRNA_TGCTTGGTCGGCACTGATAG.txt"
 #' allele_table_fp <- "/Users/timbarry/research_offsite/external/bauer-lab/rhampseq_bcl11a/hiseq1_crispresso_output/GE0423-edited/CRISPResso_on_1450_OT_0000/Alleles_frequency_table.txt"
 #' allele_feature_table <- process_crispresso_allele_table_cas9(allele_table_fp)
+#'
+#' # TO-DO: try to add position of the mutation relative to the cut site (whatever that might mean).
+#' # Note: this field would require us to know the cutsite.
 process_crispresso_allele_table_cas9 <- function(allele_table_fp) {
-  allele_tab <- readr::read_delim(allele_table_fp, col_types = "ccliiidd")
-  colnames(allele_tab) <- c("aligned_seq", "ref_seq", "unedited", "n_deleted", "n_inserted", "n_mutated", "n_reads", "percent_reads")
+  allele_tab <- readr::read_delim(allele_table_fp, col_types = c("cccciiiid"))
+  colnames(allele_tab) <- c("aligned_seq", "ref_seq", "ref_name", "status", "n_deleted", "n_inserted", "n_mutated", "n_reads", "percent_reads")
   allele_tab$allele_id <- paste0(allele_tab$aligned_seq, ":", allele_tab$ref_seq)
 
   # 1. separate mutated from unmutated alleles (as determined by CRISPResso)
-  unmutated_tab <- allele_tab |> dplyr::filter(unedited)
-  mutated_tab <- allele_tab |> dplyr::filter(!unedited)
+  unmutated_tab <- allele_tab |> dplyr::filter(status == "UNMODIFIED")
+  mutated_tab <- allele_tab |> dplyr::filter(status == "MODIFIED")
 
   if (nrow(mutated_tab) >= 1L) {
     # 3. determine mutation type
@@ -36,57 +38,20 @@ process_crispresso_allele_table_cas9 <- function(allele_table_fp) {
 
     # 4. determine mutation length
     mutation_lengths_new <- mutated_tab$n_deleted + mutated_tab$n_inserted
-
-    # 5. determine starting point of mutation (relative to cut site)
-    aligned_seq_list <- strsplit(mutated_tab$aligned_seq, split = "")
-    ref_seq_list <- strsplit(mutated_tab$ref_seq, split = "")
-    window_size <- length(aligned_seq_list[[1]])
-    quant_window_left_bdy <- window_size/2L - 1L
-    quant_window_right_bdy <- window_size/2L + 2L
-    cut_site <- window_size/2L
-    leftmost_indel_pos <- sapply(X = seq(1L, nrow(mutated_tab)), FUN = function(i) {
-      s <- if (mutation_types_new[i] == "insertion") {
-        find_leftmost_indel_pos(s = ref_seq_list[[i]], quant_window_left_bdy = quant_window_left_bdy, quant_window_right_bdy = quant_window_right_bdy)
-      } else if (mutation_types_new[i] == "deletion") {
-        find_leftmost_indel_pos(s = aligned_seq_list[[i]], quant_window_left_bdy = quant_window_left_bdy, quant_window_right_bdy = quant_window_right_bdy)
-      } else { # complex
-        min(find_leftmost_indel_pos(s = aligned_seq_list[[i]], quant_window_left_bdy = quant_window_left_bdy, quant_window_right_bdy = quant_window_right_bdy),
-            find_leftmost_indel_pos(s = ref_seq_list[[i]], quant_window_left_bdy = quant_window_left_bdy, quant_window_right_bdy = quant_window_right_bdy))
-      }
-      s - cut_site
-    })
     mutated_tab_out <- data.frame(allele_id = mutated_tab$allele_id,
                                   mutation_type = mutation_types_new, mutation_length = mutation_lengths_new,
-                                  leftmost_indel_pos = leftmost_indel_pos, read_count = mutated_tab$n_reads,
-                                  modified = TRUE)
+                                  read_count = mutated_tab$n_reads, mutated = TRUE)
   } else {
     mutated_tab_out <- data.frame()
   }
 
   # 6. prepare the output data frame
   out_df <- rbind(
-    data.frame(allele_id = "unmutated", mutation_type = NA_character_,  mutation_length = NA,
-               leftmost_indel_pos = NA, read_count = sum(unmutated_tab$n_reads),
-               modified = FALSE),
+    data.frame(allele_id = "unmutated", mutation_type = NA_character_,
+               mutation_length = NA_integer_,
+               read_count = sum(unmutated_tab$n_reads),
+               mutated = FALSE),
     mutated_tab_out
     )
   return(out_df)
-}
-
-find_leftmost_indel_pos <- function(s, quant_window_left_bdy, quant_window_right_bdy) {
-  # find the contiguous hyphen substrings
-  hyphen_idxs <- sort(grep(pattern = "-", fixed = TRUE, x = s))
-  breaks <- c(TRUE, diff(hyphen_idxs) != 1)
-  groups <- cumsum(breaks)
-  n_groups <- max(groups)
-  substring_idxs <- lapply(X = seq(1L, n_groups), FUN = function(i) hyphen_idxs[groups == i])
-
-  # for each substring, determine (i) its leftmost position and (ii) whether it overlaps the quantification window
-  substring_info <- sapply(X = substring_idxs, FUN = function(curr_substring) {
-    overlaps_quant_window <- any(curr_substring >= quant_window_left_bdy & curr_substring <= quant_window_right_bdy)
-    leftmost_pos <- min(curr_substring)
-    c(overlaps_quant_window = overlaps_quant_window, leftmost_pos = leftmost_pos)
-  }) |> t()
-  leftmost_indel_pos <- min(substring_info[substring_info[,1L] == 1L, 2])
-  return(leftmost_indel_pos)
 }
